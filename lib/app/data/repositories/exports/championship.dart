97b96d89b374c3d480c69ed5ace997248bc33256
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-06-17 14:14:59 -0300

    final

--- lib/app/data/repositories/exports/championship.dart
+++ lib/app/data/repositories/exports/championship.dart
@@ -1,4 +1,4 @@
-export '../../core/log/developer_log.dart';
-export '../../core/rest/rest_client.dart';
-export '../models/championship_model.dart';
-export 'championship_repository.dart';
+export '../../../core/log/developer_log.dart';
+export '../../../core/rest/rest_client.dart';
+export '../../models/championship_model.dart';
+export '../championship_repository.dart';
