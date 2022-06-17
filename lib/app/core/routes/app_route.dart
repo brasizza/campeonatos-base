34725eedbdb5924b3e25c6933a1de0be27c4763f
Marcus Brasizza <mvbdesenvolvimento@gmail.com>
2022-06-17 13:34:46 -0300

    routes

---
+++ lib/app/core/routes/app_route.dart
@@ -0,0 +1,73 @@
+import 'package:campeonatos/app/core/widgets/error.dart';
+import 'package:flutter/material.dart';
+import '../../data/models/competition_model.dart';
+import '../../data/repositories/championship_repository_impl.dart';
+import '../../data/repositories/competition_repository_impl.dart';
+import '../../data/services/championship_service_impl.dart';
+import '../../data/services/competition_service_impl.dart';
+import '../../modules/competitions/competition_page.dart';
+import '../../modules/home/controller/home_controller.dart';
+import '../../modules/home/home_page.dart';
+import '../../modules/leagues/controller/leagues_controller.dart';
+import '../../modules/leagues/leagues_page.dart';
+import '../../modules/splash/splash_page.dart';
+import '../rest/dio/dio_rest_client.dart';
+
+class AppRoute extends Route {
+  static Route<dynamic> routes(RouteSettings settings) {
+    final DioRestClient rest = DioRestClient.instance;
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          builder: (context) {
+            return const SplashPage();
+          },
+        );
+
+      case '/home':
+        final repository = CompetitionRepositoryImpl.instance(restClient: rest);
+        final service = CompetitionServiceImpl.instance(repository: repository);
+        final controller = HomeController.instance(service: service);
+        return MaterialPageRoute(
+          builder: (context) {
+            return HomePage(
+              controller: controller,
+              arguments: settings.arguments,
+            );
+          },
+        );
+
+      case '/competitions':
+        final arguments = settings.arguments as Map;
+        return MaterialPageRoute(
+          builder: (context) {
+            return CompetitionPage(
+              competitions: arguments['competitions'] ?? [],
+              country: arguments['country'],
+              // arguments: settings.arguments,
+            );
+          },
+        );
+
+      case '/leagues':
+        final competiton = settings.arguments as Competition;
+        final repository = ChampionshipRepositoryImpl.instance(restClient: rest);
+        final service = ChampionshipServiceImpl.instance(repository: repository);
+        final controller = LeaguesController.instance(service: service)..competition = competiton;
+        return MaterialPageRoute(
+          builder: (context) {
+            return LeaguesPage(
+              controller: controller,
+            );
+          },
+        );
+
+      default:
+        return MaterialPageRoute(
+          builder: (context) {
+            return const ErrorWidgetCustom(text: 'Rota não encontrada');
+          },
+        );
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,10 @@
 import 'package:flutter/material.dart';
-import '../../app/core/rest/dio/dio_rest_client.dart';
-import 'app/data/models/competition_model.dart';
-import 'app/data/repositories/championship_repository.dart';
-import 'app/data/repositories/championship_repository_impl.dart';
-import 'app/data/repositories/competition_repository.dart';
-import 'app/data/repositories/competition_repository_impl.dart';
-import 'app/data/services/championship_service.dart';
-import 'app/data/services/championship_service_impl.dart';
-import 'app/data/services/competition_service.dart';
-import 'app/data/services/competition_service_impl.dart';
-import '../../app/modules/competitions/competition_page.dart';
-import '../../app/modules/home/controller/home_controller.dart';
-import '../../app/modules/home/home_page.dart';
-import '../../app/modules/leagues/leagues_page.dart';
-import '../../app/modules/splash/splash_page.dart';
-import 'app/modules/leagues/controller/leagues_controller.dart';
+import 'app/core/routes/app_route.dart';
 
 void main() {
-  final DioRestClient rest = DioRestClient.instance;
-  // final HttpRestClient _rest = HttpRestClient.instance;
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     initialRoute: '/',
-    onGenerateRoute: (settings) {
-      switch (settings.name) {
-        case '/':
-          return MaterialPageRoute(
-            builder: (context) {
-              return const SplashPage();
-            },
-          );
-
-        case '/home':
-          final CompetitionRepository repository = CompetitionRepositoryImpl.instance(restClient: rest);
-          final CompetitionService service = CompetitionServiceImpl.instance(repository: repository);
-          final HomeController controller = HomeController.instance(service: service);
-          return MaterialPageRoute(
-            builder: (context) {
-              return HomePage(
-                controller: controller,
-                arguments: settings.arguments,
-              );
-            },
-          );
-
-        case '/competitions':
-          final arguments = settings.arguments as Map;
-          return MaterialPageRoute(
-            builder: (context) {
-              return CompetitionPage(
-                competitions: arguments['competitions'] ?? [],
-                country: arguments['country'],
-                // arguments: settings.arguments,
-              );
-            },
-          );
-
-        case '/leagues':
-          final competiton = settings.arguments as Competition;
-          final ChampionshipRepository repository = ChampionshipRepositoryImpl.instance(restClient: rest);
-          final ChampionshipService service = ChampionshipServiceImpl.instance(repository: repository);
-          final LeaguesController controller = LeaguesController.instance(service: service)..competition = competiton;
-          return MaterialPageRoute(
-            builder: (context) {
-              return LeaguesPage(
-                controller: controller,
-              );
-            },
-          );
-
-        default:
-          return MaterialPageRoute(
-            builder: (context) {
-              return const Text('SEM ROTA!');
-            },
-          );
-      }
-    },
+    onGenerateRoute: AppRoute.routes,
   ));
 }
